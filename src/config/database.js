05c97ba740048bf5ae932ed6ae442673e7055c04
Prachi Gour <gourprachi330@gmail.com>
2026-01-21 17:22:57 +0530

    added hosted url

--- src/config/database.js
+++ src/config/database.js
@@ -4,9 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  // process.env.DB_NAME,
+  // process.env.DB_USER,
+  // process.env.DB_PASSWORD,
+  process.env.DATABASE_URL,
   {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
